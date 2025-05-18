2d2e12d685961d8d454a03872bdc97b376d4e4d0
Paul_Mirzoyants <pavcnfh@yandex.ru>
2025-05-18 10:39:00 +0300

    fix

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -6,57 +6,44 @@ import styles from './Gallery.module.scss';
 import { useNavigate } from 'react-router-dom';
 
 const Gallery = ({ slides, isVideo }) => {
-  const navigate = useNavigate();
-  const [sliderRef, slider] = useKeenSlider({
-    loop: true,
-    slides: {
-      perView: 3,
-      spacing: 60,
-    },
-    mode: 'free-snap',
-  });
+    const navigate = useNavigate();
+    const [sliderRef, slider] = useKeenSlider({
+        loop: true,
+        slides: {
+            perView: 5,
+            spacing: 15,
+        },
+        mode: 'free-snap',
+    });
 
-  const handleSlideClick = (link) => {
-    navigate(link);
-  };
+    const handleSlideClick = link => {
+        navigate(link);
+    };
 
-  return (
-    <div className={styles.container}>
-      <img
-        onClick={() => slider.current.prev()}
-        className={styles.arrow}
-        src="/img/arrow.png"
-        alt=""
-      />
-      <div ref={sliderRef} className={`keen-slider ${styles.slider}`}>
-        {slides.map((slide) => (
-          <div
-            key={slide.id}
-            className={`keen-slider__slide ${styles.slide}`}
-            onClick={() => handleSlideClick(slide.link)}
-          >
-            <div className={styles.imageContainer}>
-              {isVideo ? (
-                <img src={slide.src} alt={slide.title} className={styles.video} />
-              ) : (
-                <img src={slide.src} alt={slide.title} className={styles.image} />
-              )}
+    return (
+        <div className={styles.container}>
+            <img onClick={() => slider.current.prev()} className={styles.arrow} src="/img/arrow.png" alt="" />
+            <div ref={sliderRef} className={`keen-slider ${styles.slider}`}>
+                {slides.map(slide => (
+                    <>
+                        <div key={slide.id} className={`keen-slider__slide ${styles.slide}`} onClick={() => handleSlideClick(slide.link)}>
+                            {isVideo ? (
+                                <img src={slide.src} alt={slide.title} className={styles.video} />
+                            ) : (
+                                <img src={slide.src} alt={slide.title} className={styles.image} />
+                            )}
+                        </div>
+                        <div className={styles.bottom}>
+                            <div className={styles.title}>{slide.name}</div>
+                            <div className={styles.subtitle}>{slide.title}</div>
+                            {/* <button className={styles.detail}>Подробнее</button> */}
+                        </div>
+                    </>
+                ))}
             </div>
-            <div className={styles.bottom}>
-              <div className={styles.title}>{slide.name}</div>
-              <div className={styles.subtitle}>{slide.title}</div>
-            </div>
-          </div>
-        ))}
-      </div>
-      <img
-        onClick={() => slider.current.next()}
-        className={`${styles.arrow} ${styles.arrowRight}`}
-        src="/img/arrow.png"
-        alt=""
-      />
-    </div>
-  );
+            <img onClick={() => slider.current.next()} className={`${styles.arrow} ${styles.arrowRight}`} src="/img/arrow.png" alt="" />
+        </div>
+    );
 };
 
 export default Gallery;
